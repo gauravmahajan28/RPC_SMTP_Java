d3e39ce01c90a7114527478c50e5897076979a6e
gaurav <gaurav.mahajan@students.iiit.ac.in>
2018-01-15 12:12:54 +0530

    adding file

---
+++ src/server/SmptServer.java
@@ -0,0 +1,25 @@
+package server;
+
+import javax.mail.Transport;
+
+public class SmptServer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		RPCResponse rpcResponse = null;
+		try
+		{
+			HttpSmptServerImplementation httpSmptServerImplementation = new HttpSmptServerImplementation();
+			httpSmptServerImplementation.readProperties();
+			rpcResponse = httpSmptServerImplementation.listenForRPCEmail();
+			httpSmptServerImplementation.sendRPCResponse(rpcResponse);
+	
+		}
+		catch(Exception e)
+		{
+			System.out.println("error occured :" + e.getMessage());
+		}
+		
+	}
+
+}
