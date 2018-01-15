8af60873422aa80f7e0af119e800273e3cae796a
gaurav <gaurav.mahajan@students.iiit.ac.in>
2018-01-15 12:19:58 +0530

    adding file

--- src/server/HttpSmptServer.java
+++ src/server/HttpSmptServer.java
@@ -1,5 +1,17 @@
 package server;
 
 public interface HttpSmptServer {
+	
+	public void readProperties() throws Exception;
+	
+	public int add(int a, int b) throws Exception;
+	
+	public RPCResponse listenForRPCEmail() throws Exception;
+	
+	public void sendRPCResponse(RPCResponse rpcResponse) throws Exception;
+	
+	public RPCRequest parseRPCRequest(String message) throws Exception;
+	
+	
 
 }
